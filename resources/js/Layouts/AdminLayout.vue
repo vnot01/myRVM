77c3376ff57e74b285c29ceaba128c3debc8a594
vnot01 <feri.febria@gmail.com>
2025-05-15 21:52:12 +0700

    Desain Manajemen User & Tema Dashboard Admin 1

--- resources/js/Layouts/AdminLayout.vue
+++ resources/js/Layouts/AdminLayout.vue
@@ -153,7 +153,7 @@ watch(() => page.props.flash, (newFlashValue, oldFlashValue) => {
                 </div>
                 <nav class="flex-grow p-4 space-y-1">
                     <NavLink :href="route('admin.dashboard')" :active="page.url.startsWith('/admin/dashboard')">
-                        <svg class="mr-3 h-6 w-6 text-gray-400 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-gray-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
+                        <svg class="mr-2 h-6 w-6 text-gray-400 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-gray-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
                               <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"/>
                               <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z"/>
                           </svg>
@@ -161,14 +161,14 @@ watch(() => page.props.flash, (newFlashValue, oldFlashValue) => {
                     </NavLink>
 
                     <NavLink :href="route('admin.rvms.index')" :active="page.url.startsWith('/admin/rvms')" v-if="page.props.auth.user.role === 'Admin'">
-                        <svg class="mr-3 w-[27px] h-[27px] text-gray-400 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-gray-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                        <svg class="mr-2 w-6 h-6 text-gray-400 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-gray-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
                             <path fill-rule="evenodd" d="M5.535 7.677c.313-.98.687-2.023.926-2.677H17.46c.253.63.646 1.64.977 2.61.166.487.312.953.416 1.347.11.42.148.675.148.779 0 .18-.032.355-.09.515-.06.161-.144.3-.243.412-.1.111-.21.192-.324.245a.809.809 0 0 1-.686 0 1.004 1.004 0 0 1-.324-.245c-.1-.112-.183-.25-.242-.412a1.473 1.473 0 0 1-.091-.515 1 1 0 1 0-2 0 1.4 1.4 0 0 1-.333.927.896.896 0 0 1-.667.323.896.896 0 0 1-.667-.323A1.401 1.401 0 0 1 13 9.736a1 1 0 1 0-2 0 1.4 1.4 0 0 1-.333.927.896.896 0 0 1-.667.323.896.896 0 0 1-.667-.323A1.4 1.4 0 0 1 9 9.74v-.008a1 1 0 0 0-2 .003v.008a1.504 1.504 0 0 1-.18.712 1.22 1.22 0 0 1-.146.209l-.007.007a1.01 1.01 0 0 1-.325.248.82.82 0 0 1-.316.08.973.973 0 0 1-.563-.256 1.224 1.224 0 0 1-.102-.103A1.518 1.518 0 0 1 5 9.724v-.006a2.543 2.543 0 0 1 .029-.207c.024-.132.06-.296.11-.49.098-.385.237-.85.395-1.344ZM4 12.112a3.521 3.521 0 0 1-1-2.376c0-.349.098-.8.202-1.208.112-.441.264-.95.428-1.46.327-1.024.715-2.104.958-2.767A1.985 1.985 0 0 1 6.456 3h11.01c.803 0 1.539.481 1.844 1.243.258.641.67 1.697 1.019 2.72a22.3 22.3 0 0 1 .457 1.487c.114.433.214.903.214 1.286 0 .412-.072.821-.214 1.207A3.288 3.288 0 0 1 20 12.16V19a2 2 0 0 1-2 2h-6a1 1 0 0 1-1-1v-4H8v4a1 1 0 0 1-1 1H6a2 2 0 0 1-2-2v-6.888ZM13 15a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-2Z" clip-rule="evenodd"/>
                           </svg>
                         Manajemen RVM
                     </NavLink>
 
                     <NavLink :href="route('admin.users.index')" :active="page.url.startsWith('/admin/users')" v-if="page.props.auth.user.role === 'Admin'">
-                         <svg class="mr-5 w-5 h-5 text-gray-400 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-gray-300"
+                         <svg class="mr-3 w-5 h-5 text-gray-400 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-gray-300"
                             aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
                               <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z"/>
                           </svg>
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,22 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                'brand-teal': '#006A6A', // Sesuaikan HEX dengan Teal Anda (misalnya)
+                'brand-olive': '#999933', // Sesuaikan HEX dengan Olive Anda (misalnya)
+                'brand-gray': '#808080',  // Sesuaikan HEX dengan Abu-abu Anda (misalnya)
+
+                // Variasi untuk light/dark mode (bisa disesuaikan lebih lanjut)
+                'light-bg': '#F3F4F6',      // Abu-abu sangat terang (slate-100/gray-100)
+                'light-card-bg': '#FFFFFF', // Putih
+                'light-text': '#1F2937',    // Abu-abu gelap (gray-800)
+                'light-text-secondary': '#6B7280', // Abu-abu sedang (gray-500)
+
+                'dark-bg': '#111827',       // Abu-abu sangat gelap (gray-900)
+                'dark-card-bg': '#1F2937',  // Abu-abu lebih terang (gray-800)
+                'dark-text': '#F3F4F6',     // Abu-abu sangat terang (gray-100/slate-100)
+                'dark-text-secondary': '#9CA3AF', // Abu-abu sedang (gray-400)
+            },
         },
     },
 
